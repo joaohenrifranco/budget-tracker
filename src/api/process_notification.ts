4afe39f5410c0bec6568c5bbabe8fc0c50d41293
joaohenrifranco <joaohenrique.franco@gmail.com>
2022-02-17 00:14:50 -0300

    Add template basics

--- src/api/process_notification.ts
+++ src/api/process_notification.ts
@@ -1,21 +1,55 @@
-// const Airtable = require('airtable')
-// /** The next 2 lines refer to environment variables configured in Netlify settings (found in "Site settings > Build & deploy > Environment" as of this writing) */
-// const { AIRTABLE_API_KEY } = process.env
-// const { AIRTABLE_BASE_ID } = process.env
-// const at_base = new Airtable({
-// 		apiKey: AIRTABLE_API_KEY
-// 	})
-// 	.base(AIRTABLE_BASE_ID)
-// const at_table_movies = at_base('movies')
+import { Handler } from "@netlify/functions";
 
-export async function handler(event, context, callback) {
-    return {
-        "statusCode": 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            event,
-            context,
-            callback,
-        })
-    }
+const variables = {
+    amount: "amount",
+    description: "description",
+    cardNumber: "cardNumber",
+    date: "date",
+    time: "time",
+    remainingCredit: "remainingCredit",
 }
+
+const payment_methods = [
+    {
+        label: "Nuconta",
+        appName: "Nubank",
+        templates: {
+            expense: "Compra de R$ [[${variables.amount}]] APROVADA em [[${variables.description}]]",
+            refund: "A compra em [[${variables.description}]] no valor de R$ [[${variables.amount}]] foi estornada."
+        }
+    },
+    {
+        label: "Nubank",
+        appName: "Nubank",
+        templates: {
+            revenue: "Você recebeu uma transferência de R$ [[${variables.amount}]] de [[${variables.description}]]."
+        }
+    },
+    {
+        label: "Itaú PDA",
+        template: {
+            expense: "Compra aprovada no seu PAO ACUCAR VS PLAT final [[${variables.cardNumber}]] - [[${variables.description}]] valor RS {{}} em [[${variables.date}]] as [[${variables.time}]]. Limite Disponivel de {{remainingCredit}}.",
+            refund: "Confirmamos o estorno da compra no seu PAO ACUCAR VS PLAT final [[${variables.cardNumber}]] - [[${variables.description}]] valor RS [[${variables.amount}]] em [[${variables.date}]] as [[${variables.time}]]."
+        }
+    },
+    {
+        label: "Caixa",
+        template: {
+            expense: "CAIXA: Compra aprovada [[${variables.description}]] R$ [[${variables.amount}]] [[${variables.date}]] as [[${variables.time}]], VISA final [[${variables.cardNumber}]]. Caso nao reconheca a transacao, envie BL1759 p/cancelar cartao"
+        }
+    },
+
+]
+
+function match() {
+
+}
+
+const handler: Handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello World" }),
+    };
+};
+
+export { handler };
