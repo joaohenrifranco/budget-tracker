a8c0ca5688e49621459d1b52c171601c654a543b
joaohenrifranco <joaohenrique.franco@gmail.com>
2022-03-13 19:08:29 -0300

    Build add-to-sheets

--- src/services/add-to-sheets.ts
+++ src/services/add-to-sheets.ts
@@ -1,87 +1,45 @@
 import { google } from 'googleapis';
-import fs from 'fs';
-import readline from 'readline';
+import { gapiCredentials } from '../config/gapi-credentials';
 
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-const TOKEN_PATH = 'token.json';
-
-
-function authorize(credentials: any, callback: any) {
-    const {client_secret, client_id, redirect_uris} = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-  
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-      if (err) return getNewToken(oAuth2Client, callback);
-      oAuth2Client.setCredentials(JSON.parse(token as any));
-      callback(oAuth2Client);
+async function createSheetsInstance(credentials: typeof gapiCredentials) {
+    const auth = new google.auth.GoogleAuth({
+        credentials,
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
-  } 
-
-function init() {
-    fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    authorize(JSON.parse(content as any), listMajors);
-  });
-
-}
-function getNewToken(oAuth2Client: any, callback: anu) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err: any, token: any) => {
-      if (err) return console.error('Error while trying to retrieve access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-function listMajors(auth) {
-  const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const rows = res.data.values;
-    if (rows.length) {
-      console.log('Name, Major:');
-      // Print columns A and E, which correspond to indices 0 and 4.
-      rows.map((row) => {
-        console.log(`${row[0]}, ${row[4]}`);
-      });
-    } else {
-      console.log('No data found.');
-    }
-  });
+    const authClient = await auth.getClient();
+    return google.sheets({ version: 'v4', auth: authClient });
 }
 
 async function execute(dataDict: { [key: string]: string }) {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "keys.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets", 
-    });
-    
-    const authClientObject = await auth.getClient();
+    const sheetsInstance = await createSheetsInstance(gapiCredentials);
 
-    const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
+    const request = {
+        spreadsheetId: process.env.SPREADSHEET_ID,
+        range: 'A1:Z9999',
+        valueInputOption: 'USER_ENTERED',
+        insertDataOption: 'INSERT_ROWS',
+        resource: {
+            values: [
+                dataDict.date, 
+                dataDict.method, 
+                dataDict.type, 
+                "", 
+                "", 
+                dataDict.description, 
+                dataDict.amount
+            ]
+        },
 
-    return null;
+        // auth: authClient,
+    };
+
+    try {
+        const response = (await sheetsInstance.spreadsheets.values.append(request)).data;
+        console.log(JSON.stringify(response, null, 2));
+    } catch (err) {
+        console.error(err);
+    }
+
 }
 
 export const AddToSheets = { execute };
