216efef71073df6440af887b8a1c716fa1e80d4a
joaohenrifranco <joaohenrique.franco@gmail.com>
2022-03-13 14:00:55 -0300

    Organize files

--- src/api/process_notification.ts
+++ src/api/process_notification.ts
@@ -1,6 +1,6 @@
-import { Handler } from `@netlify/functions`;
+import { Handler, APIGatewayEvent, Context } from `@netlify/functions`;
 
-const handler: Handler = async (event, context) => {
+const handler: Handler = async (event: APIGatewayEvent, context: Context) => {
     return {
         statusCode: 200,
         body: JSON.stringify({ message: `Hello World` }),
